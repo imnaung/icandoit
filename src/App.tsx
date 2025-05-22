34c2b5cd1ba17a50bee263971cf430cd861223bd
kny <kny@hunet.co.kr>
2025-05-22 16:26:34 +0900

    HashRouter 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Carrier from "./pages/Carrier";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/carrier" element={<Carrier />} />
-      </Routes>
-    </Router>
+    <Home />
   );
 }
 
